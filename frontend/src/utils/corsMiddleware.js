bfcd10a11b1ee4d919f67ab5e164917b7beb4c45
Egor Sabyanin <egor.sabyanin.dev@gmail.com>
2023-06-03 14:06:05 +0700

    feat: добавил поддержку env

---
+++ frontend/src/utils/corsMiddleware.js
@@ -0,0 +1,34 @@
+// Backend API: backend-mesto-es.nomoredomains.rocks
+
+const allowedCors = [
+  'localhost',
+  'localhost:3001',
+  'http://localhost',
+  'http://localhost:3001',
+  'http://localhost:3001/',
+];
+
+const corsMiddleware = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+
+    if (allowedCors.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
+
+    return res.end();
+  }
+
+  return next();
+};
+
+module.exports = {
+  allowedCors,
+  corsMiddleware,
+};
